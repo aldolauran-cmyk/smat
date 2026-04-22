61819f386ccda1d6718a0042d78a18e401d10edb
laura ñahuinripa aldo <su.aldo.lauran@unmsm.edu.pe>
2026-04-22 12:03:28 -0500

    Limpieza de interfaz Swagger y ajuste de metadatos

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from . import models, schemas, crud
@@ -6,7 +6,11 @@ from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="SMAT Backend")
+app = FastAPI(
+    title="SMAT",
+    description="Sistema de Monitoreo de Alerta Temprana",
+    version="1.0.1"
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,14 +19,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.post("/estaciones/", response_model=schemas.Estacion)
+@app.get("/", tags=["Inicio"])
+def read_root():
+    return {"message": "Servidor Activo"}
+
+@app.post("/estaciones/", tags=["Infraestructura"])
 def crear_estacion(estacion: schemas.EstacionCreate, db: Session = Depends(get_db)):
     return crud.crear_estacion(db=db, estacion=estacion)
 
-@app.post("/lecturas/", response_model=schemas.Lectura)
+@app.post("/lecturas/", tags=["Infraestructura"])
 def registrar_lectura(lectura: schemas.LecturaCreate, db: Session = Depends(get_db)):
     return crud.crear_lectura(db=db, lectura=lectura)
 
-@app.get("/estaciones/stats")
+@app.get("/estaciones/stats", tags=["Auditoria"])
 def obtener_estadisticas(db: Session = Depends(get_db)):
     return crud.obtener_stats(db)
